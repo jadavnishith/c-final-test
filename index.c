90f78e367afd3e41515b57079d386b9d9c147f89
jadavnishith <mrnick076@gmail.com>
2024-02-08 12:58:03 +0530

    final exam

--- index.c
+++ index.c
@@ -83,7 +83,31 @@
 
 //---------- Question 4-----------
 #include <stdio.h>
-int main(){
+
+void squareArray(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        (arr + i) = ((arr + i)) * (*(arr + i));
+    }
+}
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    squareArray(arr, size);
+
+    printf("Square of each element:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 
     return 0;
 }
